b48b7ebb336346596dbde11e592f9d5e2d7613dd
kmchen <kuangmingchen0702@gmail.com>
2016-05-09 16:21:42 +0800

    Init commit of double columns React

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+//var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,          
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    },
+    { test: /\.css$/,
+      loader: 'style!css!autoprefixer?browsers=last 2 versions'
+    }] 
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']          
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+}
